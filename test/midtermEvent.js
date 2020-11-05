63b491fff4f9f92a82f8d5091a6ce9d21ab2dde4
i-giron <57995418+i-giron@users.noreply.github.com>
2020-11-05 18:07:24 -0500

    commit

--- test/midtermEvent.js
+++
@@ -1,1 +0,0 @@
-let msg 1 = document.getElementById('midtermMessage1');let msg 2 = document.getElementById('midtermMessage2');function myfunc(name) {msg2.innerHTML = msg1.textContent + (name);}button.addEventListener("click", myfunc('Idis Giron'));
